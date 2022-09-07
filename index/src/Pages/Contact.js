6da0a844a96f94eb13db5ada2ceaad81b69a3895
cpblanch <cpblanch@go.olemiss.edu>
2022-09-06 22:54:24 -0500

    making good progress! home and contact almost done full screen

--- index/src/Pages/Contact.js
+++ index/src/Pages/Contact.js
@@ -1,10 +1,25 @@
-import '../Styles/testStyles.css';
+import '../Styles/Contact.css';
+
 
 function Contact (){
     return(
-        <main>
-            <h3>Contact Page</h3>
-        </main>
+        <div className='contact-container'>
+        <div>
+            <img className = "headshot" src={require('./blanchard_headshot.png')} />
+            <img className = "orange" src={require('./orangecontact.png')} />
+        <div className = "contacttext">
+            <h1>Hi there!</h1>
+            <h2>My name is Cameron Blanchard.</h2>
+            <h3>I am a senior computer science student, and Movie Magic is my senior project!</h3>
+            <p>I chose to create this website because I didn't have any prior experience with front-end development,
+                and I wanted to expand my knowledge! Thank you to Dr. Feng Wang, Dr. Charles Walter, and Dr. Timothy Holston at the 
+                University of Mississippi for your help and guidance throughout this project!
+                If you would like more information on how this project was created, please reach out at cpblanch@go.olemiss.edu.
+            </p>
+            <h4>I hope you enjoy Movie Magic!</h4>
+        </div>
+        </div>
+        </div>
     );
 }
 
