24a2c4f1b9fde307a9c6dbd9f5dfdfdc2df801c5
cpblanch <cpblanch@go.olemiss.edu>
2022-11-09 00:59:52 -0600

    with cors

--- server/index.js
+++ server/index.js
@@ -11,13 +11,13 @@ const db = mysql.createConnection({
 });
 
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 
