d2c0e1b5fb2e39e8a18c703331b1b2bdc291146d
cpblanch <cpblanch@go.olemiss.edu>
2022-11-30 20:02:25 -0600

    all done

--- index/src/Pages/Home.js
+++ index/src/Pages/Home.js
@@ -8,15 +8,23 @@ import {useLocation} from 'react-router-dom';
 
 
 
-function Home (){
+function Home (param){
 
     //preventing returns after logout 
+    const location = useLocation();
+    console.log("here 1")
+    console.log(param)
+
+console.log(location.state)
+
+if (location.state != null){
     window.history.pushState(null, null, window.location.href);
     window.onpopstate = function (event) {
         window.history.go(1);
         window.location.reload(false)
 
     }
+}
 
 //displaying homepage video
     return(
